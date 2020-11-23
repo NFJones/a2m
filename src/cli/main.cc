bd54801ae2913896778109017a74aeef35c56f6b
Neil Jones <neil.franklin.jones@gmail.com>
2020-11-23 13:11:55 -0600

    init

---
+++ src/cli/main.cc
@@ -0,0 +1,5 @@
+#include <a2m/converter.h>
+
+int main(int argc, char **argv) {
+    return 0;
+}
---
+++ test/test.h
@@ -0,0 +1,8 @@
+#include <cxxtest/TestSuite.h>
+
+class audio_to_midi_test_suite : public CxxTest::TestSuite {
+    public:
+    void test_dummy() {
+        TS_ASSERT(true);
+    }
+};
